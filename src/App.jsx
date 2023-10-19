67eb693791181f8e8d51c7b158264a4936dbdc82
chsh22078 <chsh22078@g.nihon-u.ac.jp>
2023-10-19 14:56:59 +0900

    reactの練習

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import Aside from "./components/Aside";
+
+
+export default function App() {
+  return (
+    <div>
+      <img src="/src/public/dove.png" />
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+      <header>
+         <h1>Birdwatching</h1>
+        <img src="public/dove.png" alt="a simple dove logo" width="100" height="100"/>
+      </header>
+    );
+  }
